06146cbd6ba4564f5246f5ebca97e67229af0fb9
thakkaha <harsh.thakkar@sheridancollege.ca>
2024-02-28 14:26:11 -0500

    Update app.js

--- app.js
+++ app.js
@@ -16,14 +16,14 @@ const html = `
     <title>Hello from Render!</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
-      setTimeout(() => {
+      conf = () => {
         confetti({
           particleCount: 100,
           spread: 70,
           origin: { y: 0.6 },
           disableForReducedMotion: true
         });
-      }, 500);
+      };
     </script>
     <style>
       @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
@@ -54,7 +54,7 @@ const html = `
   </head>
   <body>
     <section>
-      Hello from Render!
+      Hello from <span onclick="conf()">Render</span>!
     </section>
   </body>
 </html>
